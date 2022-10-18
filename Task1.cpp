813f11742b2157ecba0eb753a46884bef40c95d6
ehsanul-haque <mdehaquesiam@gmail.com>
2022-10-18 17:43:06 +0600

    Task1

--- Task1.cpp
+++ Task1.cpp
@@ -55,3 +55,68 @@ int main()
 
     return 0;
 }
+/*
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, max, min;
+
+    cout << "Enter 1st Number\n";
+    cin >> a;
+    cout << "Enter 2nd Number\n";
+    cin >> b;
+    cout << "Enter 3rd Number\n";
+    cin >> c;
+
+    if (a > b)
+    {
+        if (a > c)
+            max = a;
+        else
+            max = c;
+    }
+    else
+    {
+        if (b > c)
+            max = b;
+        else
+            max = c;
+    }
+
+    if (a < b)
+    {
+        if (a < c)
+            min = a;
+        else
+            min = c;
+    }
+    else
+    {
+        if (b < c)
+            min = b;
+        else
+            min = c;
+    }
+    if (max % 2 == 0)
+    {
+        cout << "Largest number is : " << max << ", it is even" << endl;
+    }
+    else
+    {
+        cout << "Largest number is : " << max << ", it is odd" << endl;
+    }
+    if (min % 2 == 0)
+    {
+        cout << "smallest number is : " << min << ", it is even" << endl;
+    }
+    else
+    {
+        cout << "smallest number is : " << min << ", it is odd" << endl;
+    }
+
+    return 0;
+}
+*/
